9ac68c4a470889a85d2fe4a888e6cc099f224a44
ArChanDD <povla13@gmail.com>
2023-09-01 21:24:36 +0300

    Add a lot of

--- RandomCoffee.py
+++ RandomCoffee.py
@@ -1,40 +1,76 @@
 from telebot import types
 import numpy as np
 import json
-from itertools import combinations
 
 
 class RandomCoffee:
-    def __init__(self, load_files=False):
+    """Класс для всех операций с системой Random Coffee"""
+
+    def __init__(self, load_files: bool = False):
+        """
+        :param load_files: True для повторной иницализации, либо иного случая когда файлы с данными о пользователях уже есть
+        """
+        self.MAX_PERVAK_PER_KURATOR = 3
         self.user_preferences = {}
         self.type_of_user = {}
         self.id_to_fac = {}
-        self.fac_to_ids = {"Математика": [], "Современное Программирование": [], "Науки о Данных": []}
+        self.total_id_to_fac = self.id_to_fac.copy()
         self.facs = ["Математика", "Современное Программирование", "Науки о Данных"]
         self.valid_choises = ['Однокурсники', 'Кураторы', 'Другое направление']
         self.total_choise_variants = self.valid_choises + ['Готово']
         self.ref_cast = {'Однокурсники': 'odnokur', 'Кураторы': 'kurator', 'Готово': 'choose_done',
                          'Другое направление': 'other_fac'}
-        self.preferences_cast = {v:k for k,v in self.ref_cast.items()}
+        self.preferences_cast = {v: k for k, v in self.ref_cast.items()}
         if load_files:
-            with open('files/id_to_fac.json', 'r') as f:
-                self.id_to_fac = json.load(f)
-            with open('files/fac_to_ids.json', 'r') as f:
-                self.fac_to_ids = json.load(f)
+            with open('files/total_id_to_fac.json', 'r') as f:
+                self.total_id_to_fac = {int(k): v for k, v in json.load(f).items()}
+            with open('files/type_of_user.json', 'r') as f:
+                self.type_of_user = {int(k): v for k, v in json.load(f).items()}
 
-    def add_user(self, user_id):
+    def add_user(self, user_id: int):
+        """Метод добавления нового пользователя
+        :param user_id: message.chat.id для нужного чата"""
         self.user_preferences[user_id] = []
 
-    def remove_user(self, user_id):
+    def remove_user(self, user_id: int):
+        """
+        Удаление информации о пользователе
+        :param user_id: message.chat.id для нужного чата
+        :return:
+        """
         try:
             del self.user_preferences[user_id]
         except:
             pass
 
-    def set_status_pervak(self, user_id):
+    def set_status_pervak(self, user_id: int):
+        """
+        Установление статуса первокурсника для `user_id`
+        :param user_id: message.chat.id для нужного чата
+        :return:
+        """
         self.type_of_user[user_id] = 'child'
+        with open('files/type_of_user.json', 'w') as f:
+            json.dump(self.type_of_user, f)
 
-    def add_preference(self, user_id, pref):
+    def set_status_kurator(self, user_id: int):
+        """
+        Установление статуса куратора для `user_id`, а также всех политик для куратора
+        :param user_id: message.chat.id для нужного чата
+        :return:
+        """
+        self.type_of_user[user_id] = 'kurator'
+        self.user_preferences[user_id] = ['Однокурсник']
+        with open('files/type_of_user.json', 'w') as f:
+            json.dump(self.type_of_user, f)
+
+    def add_preference(self, user_id: int, pref: str):
+        """
+        Добавление предпочтения для пользователя
+        :param user_id: message.chat.id для нужного чата
+        :param pref: Один из `odnokur`, `kurator`, `other_fac`, `choose_done`
+        :return:
+        """
         upd_pref = self.preferences_cast[pref]
         if upd_pref == 'Готово':
             return
@@ -45,19 +81,31 @@ class RandomCoffee:
         else:
             self.user_preferences[user_id].remove(upd_pref)
 
-    def get_preferences(self, user_id):
+    def get_preferences(self, user_id: int):
+        """
+        Получить список предпочтений для `user_id`
+        :param user_id: message.chat.id для нужного чата
+        :return:
+        """
         return self.user_preferences[user_id]
 
-    def update_preferences(self, user_id, new_pref):
+    def update_preferences(self, user_id: int, new_pref: str):
+        """
+        Добавить предпочтение `new_pref` для пользователя `user_id`
+        :param user_id: message.chat.id для нужного чата
+        :param new_pref: Одно из предпочтений
+        :return:
+        """
         self.add_preference(user_id, self.preferences_cast[new_pref])
 
-    def add_fac_for_user(self, user_id, fac):
-        self.id_to_fac[user_id] = fac
-        self.fac_to_ids[fac].append(user_id)
-        with open('files/id_to_fac.json', 'w') as f:
-            json.dump(self.id_to_fac, f)
-        with open('files/fac_to_ids.json', 'w') as f:
-            json.dump(self.fac_to_ids, f)
+    def add_fac_for_user(self, user_id, fac=None):
+        if fac is None:
+            self.id_to_fac[user_id] = self.total_id_to_fac[user_id]
+        else:
+            self.id_to_fac[user_id] = fac
+            self.total_id_to_fac[user_id] = fac
+            with open('files/total_id_to_fac.json', 'w') as f:
+                json.dump(self.id_to_fac, f)
 
     def get_beautiful_choises_buttons_for_user(self, user_id):
         user_preferences = self.user_preferences[user_id]
@@ -76,57 +124,163 @@ class RandomCoffee:
             markup.add(button)
         return markup
 
+    def is_user_enable(self, user_id: int):
+        """
+        Проверка на наличия пользователя `user_id` в базе
+        :param user_id: message.chat.id для нужного чата
+        :return:
+        """
+        return user_id in list(self.type_of_user.keys())
+
     def get_pairs(self):
+        fac_to_ids = {'Математика': [], 'Современное Программирование': [], 'Науки о Данных': []}
+        for user_id, fac in self.id_to_fac.items():
+            fac_to_ids[fac].append(user_id)
+
         pairs = []
+        next_time = {x: [] for x in self.facs}
+        total_students_not_flatten = [fac_to_ids[fac] for fac in self.facs]
+        total_students_to_pairing = [item for sublist in total_students_not_flatten for item in sublist if
+                                     self.type_of_user[item] == 'child']
+        fac_to_kurators = {}
         for fac in self.facs:
-            cur_users = self.fac_to_ids[fac]
-            # Only one choise
+            fac_to_kurators[fac] = [user_id for user_id in fac_to_ids[fac]
+                                    if self.type_of_user[user_id] == 'kurator']
 
-            pair_founded = []
-            for choise in ['Однокурсники']:
-                this_type_of = []
-                for k, v in self.user_preferences.items():
-                    if k in cur_users and set(v) == set(choise):
-                        this_type_of.append(k)
-                to_select = this_type_of
-                while len(to_select) > 1:
-                    pair = np.random.choice(to_select, 2, replace=False)
-                    for p in pair:
-                        to_select.remove(p)
-                        pair_founded.append(p)
-                    pairs.append(pair)
+        # child 2 child
+        for fac in self.facs:
+            pervaki_for_pair = [user_id
+                                for user_id in fac_to_ids[fac]
+                                if self.user_preferences[user_id] == ['Однокурсники']
+                                and self.type_of_user[user_id] == 'child']
+            while len(pervaki_for_pair) > 1:
+                pair = np.random.choice(pervaki_for_pair, 2, replace=False)
+                for x in pair:
+                    pervaki_for_pair.remove(x)
+                    total_students_to_pairing.remove(x)
+                pairs.append(pair)
+            if len(pervaki_for_pair) == 1:
+                next_time[fac].append(pervaki_for_pair[0])
 
-            # Two choises
+        # child 2 kurator
+        kurator_pairs_count = {}
+        for fac in self.facs:
+            kurator_pairs_count[fac] = {user_id: 0 for user_id in fac_to_kurators[fac]}
+        for fac in self.facs:
+            pervaki_for_pair = [user_id
+                                for user_id in fac_to_ids[fac]
+                                if self.user_preferences[user_id] == ['Кураторы']
+                                and self.type_of_user[user_id] == 'child']
+            kuratori_for_pair = [user_id
+                                 for user_id in fac_to_ids[fac]
+                                 if self.type_of_user[user_id] == 'kurator']
+            np.random.shuffle(pervaki_for_pair)
+            np.random.shuffle(kuratori_for_pair)
+            kur_len = len(kuratori_for_pair)
+            if kur_len == 0:
+                break
+            start_id_no_pair = len(pervaki_for_pair)
+            for i, pervak_id in enumerate(pervaki_for_pair):
+                kur_id = kuratori_for_pair[i % kur_len]
+                if kurator_pairs_count[fac][kur_id] == self.MAX_PERVAK_PER_KURATOR:
+                    start_id_no_pair = i
+                    break
+                pairs.append([pervak_id, kur_id])
+                total_students_to_pairing.remove(pervak_id)
+                kurator_pairs_count[fac][kur_id] += 1
+            for j in range(start_id_no_pair, len(pervaki_for_pair)):
+                next_time[fac].append(pervaki_for_pair[j])
 
-            for pair_choise in list(combinations(self.valid_choises, 2)):
-                this_type_of = []
-                for k, v in self.user_preferences.items():
-                    if k in cur_users and set(v) == set(pair_choise):
-                        this_type_of.append(k)
-                to_select = this_type_of
-                while len(to_select) > 1:
-                    pair = np.random.choice(to_select, 2, replace=False)
-                    for p in pair:
-                        to_select.remove(p)
-                        pair_founded.append(p)
-                    pairs.append(pair)
+        # child 2 (child+kurator) OR (child+other) OR (child+kurator+other) -> we think pair will be "child"
+        for fac in self.facs:
+            pervaki_for_pair = [user_id
+                                for user_id in fac_to_ids[fac]
+                                if (set(self.user_preferences[user_id]) == {'Однокурсники', 'Кураторы'}
+                                    or set(self.user_preferences[user_id]) == {'Однокурсники', 'Другое направление'}
+                                    or set(self.user_preferences[user_id]) == {'Однокурсники', 'Кураторы',
+                                                                               'Другое направление'})
+                                and self.type_of_user[user_id] == 'child']
+            while len(pervaki_for_pair) > 1:
+                pair = np.random.choice(pervaki_for_pair, 2, replace=False)
+                for x in pair:
+                    pervaki_for_pair.remove(x)
+                    total_students_to_pairing.remove(x)
+                pairs.append(pair)
+            if len(pervaki_for_pair) == 1:
+                next_time[fac].append(pervaki_for_pair[0])
 
-            # Kto ostalsya
+        # child 2 other OR (kurator+other) -> we think pair will be "other"
+        pervaki_for_pair = []
+        for fac in self.facs:
+            pervaki_for_pair.append([user_id
+                                     for user_id in fac_to_ids[fac]
+                                     if 'Другое направление' in self.user_preferences[user_id]
+                                     and user_id in total_students_to_pairing
+                                     and self.type_of_user[user_id] == 'child'])
+        pervaki_for_pair = [item for sublist in pervaki_for_pair for item in sublist]
+        np.random.shuffle(pervaki_for_pair)
+        pervaki_for_pair = list(pervaki_for_pair)
 
-            left_users = set(cur_users).difference(set(pair_founded))
-            if len(left_users) == 0:
-                return pairs
-            else:
-                # В текущей реализации не достаться пары (и мы обратим на это внимание) могло только перваку, который
-                # попросил первака в пару
-                # Или же первак попросил и куратора, но их не хватило.
-                # Решено тупа сделать рандомные пары из того что осталось
-                left_users_upd = list(left_users)
-                while len(left_users_upd) > 1:
-                    pair = np.random.choice(left_users_upd, 2, replace=False)
-                    for p in pair:
-                        left_users_upd.remove(p)
-                    pairs.append(pair)
-                if len(left_users_upd) == 1 and len(pair_founded) > 0:
-                    pairs.append([left_users_upd[0], np.random.choice(pair_founded, 1, replace=False)[0]])
-        return pairs
+        while len(pervaki_for_pair) > 1:
+            pair = np.random.choice(pervaki_for_pair, 2, replace=False)
+            for x in pair:
+                pervaki_for_pair.remove(x)
+                total_students_to_pairing.remove(x)
+            pairs.append(pair)
+        if len(pervaki_for_pair) == 1:
+            next_time[self.id_to_fac[pervaki_for_pair[0]]].append(pervaki_for_pair[0])
+
+        # kto ostalsya
+        not_found_pairs = []
+        to_other_napr_part_2 = []
+        for fac in self.facs:
+            while len(next_time[fac]) != 0:
+                cur_user = next_time[fac][0]
+                pair_founded = False
+                if 'Кураторы' in self.user_preferences[cur_user]:
+                    cur_kurator = None
+                    for kurator_id in fac_to_kurators[fac]:
+                        if kurator_pairs_count[fac][kurator_id] < self.MAX_PERVAK_PER_KURATOR:
+                            cur_kurator = kurator_id
+                            break
+                    if cur_kurator is not None:
+                        pairs.append([cur_user, cur_kurator])
+                        kurator_pairs_count[fac][cur_kurator] += 1
+                        next_time[fac].remove(cur_user)
+                        pair_founded = True
+                if 'Однокурсники' in self.user_preferences[cur_user] and not pair_founded:
+                    cur_user_pair = None
+                    for us in next_time[fac][1:]:
+                        if 'Однокурсники' in self.user_preferences[us]:
+                            cur_user_pair = us
+                            break
+                    if cur_user_pair is not None:
+                        pairs.append([cur_user, cur_user_pair])
+                        next_time[fac].remove(cur_user)
+                        next_time[fac].remove(cur_user_pair)
+                        pair_founded = True
+                if 'Другое направление' in self.user_preferences[cur_user] and not pair_founded:
+                    to_other_napr_part_2.append(cur_user)
+                    next_time[fac].remove(cur_user)
+                    pair_founded = True
+                if not pair_founded:
+                    not_found_pairs.append(cur_user)
+                    next_time[fac].remove(cur_user)
+
+        # different napravleniya part 2
+        maybe_pairs = to_other_napr_part_2
+
+        while len(maybe_pairs) > 1:
+            pair = np.random.choice(maybe_pairs, 2, replace=False)
+            for x in pair:
+                maybe_pairs.remove(x)
+                total_students_to_pairing.remove(x)
+            pairs.append(pair)
+        if len(maybe_pairs) == 1:
+            not_found_pairs.append(maybe_pairs[0])
+
+        for fac in self.facs:
+            for id, val in kurator_pairs_count[fac].items():
+                if val == 0:
+                    not_found_pairs.append(id)
+        return [list(x) for x in pairs], not_found_pairs
--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from threading import Thread
 from time import sleep
 from telebot import types
 from RandomCoffee import RandomCoffee
+from Logs2File import Logs2File
+import os
+import sys
 
-#TOKEN = "6402528524:AAEyRsc1SZ1LIPJt5MAoCGBpNDPdybOMuNU"
-TOKEN='6356205404:AAEXB__YD4KJ2A4CmOyUVbF0-vp1S2Bj2AM'
+TOKEN = os.environ.get("MCSRandomCoffeeTOKEN") or '6356205404:AAEXB__YD4KJ2A4CmOyUVbF0-vp1S2Bj2AM'
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -26,32 +28,38 @@ def schedule_checker():
 
 def send_update():
     global random_coffee_users, id_to_username
-    pairs = random_coffee_users.get_pairs()
-    remembered_ids = list(random_coffee_users.user_preferences.keys())
+    pairs, not_found = random_coffee_users.get_pairs()
+    try:
+        with open('files/pairs_history.txt', 'a') as f:
+            prep_pairs = [[id_to_username[p[0]], id_to_username[p[1]]] for p in pairs]
+            prep_not_found = [id_to_username[p] for p in not_found]
+            f.write('\n\n' + str(prep_pairs) + '\n' + str(prep_not_found))
+    except:
+        pass
+
     print('send pairs...')
-    for id in remembered_ids:
-        good_pairs = []
-        for p in pairs:
-            if p[0] == id or p[1] == id:
-                good_pairs.append(p)
-        if len(good_pairs) > 1:
-            bot.send_message(id, 'В этот раз я нашел тебе не одну пару!')
-        for p in good_pairs:
-            if p[0] == id:
-                text = f'На этой неделе твоя пара - @{id_to_username[p[1]]}\nС тебя встреча - в зуме, или очно, думаю вы договоритесь :)'
-            else:
-                text = f'На этой неделе твоя пара - @{id_to_username[p[0]]}\nВстреча с партнера, но можешь и ты проявить инициативу :)'
-            bot.send_message(id, text)
-        if len(good_pairs) == 0:
-            bot.send_message(id,
-                             'К сожалению, на этой неделе не смог найти для тебя пару :(\nНе переживай, на следующей неделе обязательно найдем!')
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-        markup.add(
-            types.KeyboardButton("Да"),
-            types.KeyboardButton("Нет")
-        )
-        bot.send_message(id, 'Хочешь поучаствовать в Random Coffee на следущей неделе?', reply_markup=markup)
-        bot.register_next_step_handler(message_start[id], want_to_do_it_again)
+
+    # Send if pair not found
+    for id in not_found:
+        bot.send_message(id,
+                         'Привет!\nК сожалению, на этой неделе не смог найти для тебя пару :(\nНе переживай, на следующей неделе обязательно найдем!')
+
+    # Send if pair was found
+    for p in pairs:
+        bot.send_message(p[0],
+                         'Привет!\n' + f'На этой неделе твоя пара - @{id_to_username[p[1]]}\nС тебя встреча - в зуме, или очно, думаю вы договоритесь :)')
+        bot.send_message(p[1],
+                         'Привет!\n' + f'На этой неделе твоя пара - @{id_to_username[p[0]]}\nВстреча с партнера, но можешь и ты проявить инициативу :)')
+
+    remembered_ids = list(random_coffee_users.user_preferences.keys())
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    markup.add(
+        types.KeyboardButton("Да"),
+        types.KeyboardButton("Нет")
+    )
+    for user_id in remembered_ids:
+        bot.send_message(user_id, 'Хочешь поучаствовать в Random Coffee на следущей неделе?', reply_markup=markup)
+        bot.register_next_step_handler(message_start[user_id], want_to_do_it_again)
     id_to_username = {}
     random_coffee_users = RandomCoffee(load_files=True)
 
@@ -60,13 +68,21 @@ def want_to_do_it_again(message):
     if message.text == 'Да':
         bot.send_message(message.chat.id, 'Я знал, что тебе понравится :)')
         random_coffee_users.add_user(message.chat.id)
+        random_coffee_users.add_fac_for_user(message.chat.id)
         id_to_username[message.chat.id] = message.chat.username
-        # random_coffee_users.add_fac_for_user(message.chat.id, message.text)
-        random_coffee_users.set_status_pervak(message.chat.id)
-        markup = random_coffee_users.get_preferences_markup_for_user(message.chat.id)
-        message_id = bot.send_message(message.chat.id,
-                                      'Выбери, всех, с кем бы ты хотел пообщаться, затем нажми "Готово"',
-                                      reply_markup=markup).message_id
+        # random_coffee_users.set_status_pervak(message.chat.id)
+        if random_coffee_users.type_of_user[message.chat.id] == 'kurator':
+            random_coffee_users.add_preference(message.chat.id, random_coffee_users.ref_cast['Однокурсники'])
+            markup = types.InlineKeyboardMarkup(row_width=1)
+            markup.add(types.InlineKeyboardButton('Готово', callback_data=random_coffee_users.ref_cast['Готово']))
+            message_id = bot.send_message(message.chat.id,
+                                          'Ты куратор, поэтому тебе можно общаться только с перваками))0)',
+                                          reply_markup=markup).message_id
+        else:
+            markup = random_coffee_users.get_preferences_markup_for_user(message.chat.id)
+            message_id = bot.send_message(message.chat.id,
+                                          'Выбери, всех, с кем бы ты хотел пообщаться, затем нажми "Готово"',
+                                          reply_markup=markup).message_id
         choose_message_to_edit[message.chat.id] = message_id
 
 
@@ -150,9 +166,28 @@ def stop(message):
     random_coffee_users.remove_user(chat_id)
 
 
+# KURATOR MODE
+@bot.message_handler(commands=['kurator_mode'])
+def kurator_mode(message):
+    chat_id = message.chat.id
+    if random_coffee_users.is_user_enable(chat_id):
+        random_coffee_users.set_status_kurator(chat_id)
+        if chat_id in choose_message_to_edit.keys():
+            bot.edit_message_text('Режим куратора. Предпочтение - Перваки.', chat_id,
+                                  choose_message_to_edit[chat_id])
+    else:
+        bot.send_message(chat_id, 'Для начала зарегистрируйся как участник')
+        return
+    print(f'new kurator - {chat_id}')
+    bot.send_message(chat_id,
+                     'Запомнил тебя как куратора:)\nТеперь с тобой смогут общаться только твои дети. Увы, такие правила.')
+
+
 if __name__ == "__main__":
+    sys.stdout = Logs2File('files/log.txt', 'a')
     scheduleThread = Thread(target=schedule_checker)
     scheduleThread.daemon = True
     scheduleThread.start()
-    schedule.every(1).day.at('12:00').do(send_update)
+    # schedule.every(1).day.at('12:00').do(send_update)
+    schedule.every(30).seconds.do(send_update)
     bot.polling()
